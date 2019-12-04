7c652137e188c8321b8c0148bc66ca0f1e9f4d62
Lucas <lucas@mightyhive.com>
2019-12-04 18:56:30 +0000

    day 4 part 2 unfinished

--- day04/day4.rb
+++ day04/day4.rb
@@ -1,11 +1,25 @@
 low = 178416
 high = 676461
 
-total = 0
+total_one = 0
 
 (low..high).each do |number|
-    total += 1 if number.to_s.chars.sort == number.to_s.chars && number.to_s.chars.chunk(&:itself).to_a.length != 6
+    total_one += 1 if number.to_s.chars.sort == number.to_s.chars && number.to_s.chars.chunk(&:itself).to_a.length != 6
 end
 
-p total
+#PART 1
+p total_one
         
+
+total_two = 0
+
+(low..high).each do |number|
+    if number.to_s.chars.sort == number.to_s.chars && number.to_s.chars.chunk(&:itself).to_a.length != 6
+        len = []
+        number.to_s.chars.chunk(&:itself).each { |stick, arr| len << arr.length }
+        total_two += 1 if len.last < len.max || len.last == len.min
+        p [len, len.last < len.max || len.last == len.min]
+    end
+end
+
+p total_two
