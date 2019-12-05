245d5229e41199337c7e3d8df7ef14fb66bb8e5c
Lucas Cheung <lucascheungls@gmail.com>
2019-12-05 00:01:50 +0000

    day 4 done

--- day04/day4.rb
+++ day04/day4.rb
@@ -9,17 +9,17 @@ end
 
 #PART 1
 p total_one
-        
+
 
 total_two = 0
 
 (low..high).each do |number|
     if number.to_s.chars.sort == number.to_s.chars && number.to_s.chars.chunk(&:itself).to_a.length != 6
         len = []
-        number.to_s.chars.chunk(&:itself).each { |stick, arr| len << arr.length }
-        total_two += 1 if len.last < len.max || len.last == len.min
-        p [len, len.last < len.max || len.last == len.min]
+        number.to_s.chars.chunk(&:itself).each { |stick, arr| len << arr.length if arr.length > 1}
+        total_two += 1 if len.include? 2
     end
 end
 
+#PART 2
 p total_two
