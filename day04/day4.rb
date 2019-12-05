1ed3c6778ed8f07df94cc54eb7ff0e1d069e1e95
Lucas Cheung <lucascheungls@gmail.com>
2019-12-05 00:10:13 +0000

    done day 4

--- day04/day4.rb
+++ day04/day4.rb
@@ -12,7 +12,6 @@ p total_one
 total_two = 0
 (low..high).each do |number|
     if number.to_s.chars.sort == number.to_s.chars && number.to_s.chars.chunk(&:itself).to_a.length != 6
-        len = []
         len = number.to_s.chars.chunk(&:itself).map { |stick, arr| arr.length }
         total_two += 1 if len.include? 2
     end
