336e48eb122aedf09cb9a12dd9c599d5c6e01869
Lucas <lucas@mightyhive.com>
2019-12-06 01:28:14 +0000

    done day 5

--- day05/day5.rb
+++ day05/day5.rb
@@ -4,12 +4,12 @@ File.open('day5.input').each_line do |line|
 end
 
 op_pos = 0
-# a[1] = 1
-input = 1
-# a[18] += 70
-# def output_value(pm, pos)
-#     if pm == '0'
-#         return arr[arr[pos + 1]]
+input = 5
+
+def mode_output(pm, a, op_pos, pos)
+    return a[a[op_pos + pos]] if pm == "0"
+    return a[op_pos + pos]
+end
 
 while a[op_pos].to_s[-2..-1] != "99"
     if a[op_pos].to_s[-2..-1]
@@ -20,38 +20,40 @@ while a[op_pos].to_s[-2..-1] != "99"
     first_pm = a[op_pos].to_s[-3] || "0"
     second_pm = a[op_pos].to_s[-4] || "0"
     third_pm = a[op_pos].to_s[-5] || "0"
-    if first_pm == "0"
-        first = a[a[op_pos + 1]]
-    else
-        first = a[op_pos + 1]
-    end
-    if second_pm == "0"
-        second = a[a[op_pos + 2]]
-    else
-        second = a[op_pos + 2]
-    end
-    if third_pm == "0"
-        third = a[a[op_pos + 3]]
-    else
-        third = a[op_pos + 3]
-    end
+    first = mode_output(first_pm, a, op_pos, 1)
+    second = mode_output(second_pm, a, op_pos, 2)
+    third = mode_output(third_pm, a, op_pos, 3)
     if op == "01"
         a[a[op_pos + 3]] = first + second
-        # p [op, a[a[op_pos + 3]], op_pos]
         op_pos += 4
     elsif op == "02"
         a[a[op_pos + 3]] = first * second
-        # p [op, a[a[op_pos + 3]], op_pos]
         op_pos += 4
     elsif op == "03"
         a[a[op_pos + 3]] = input
-        # p [op, a[a[op_pos + 3]], op_pos]
         op_pos += 2
     elsif op == "04"
-        # a[a[op_pos + 3]] = first
-        p [op, a[a[op_pos + 1]], op_pos]
+        p a[a[op_pos + 1]]
         op_pos += 2
-    else
-        p 'error'
+    elsif op == "05"
+        op_pos = second - 3 if first != 0
+        op_pos += 3
+    elsif op == "06"
+        op_pos = second - 3 if first == 0
+        op_pos += 3
+    elsif op == "07"
+        if first < second
+            a[a[op_pos + 3]] = 1
+        else
+            a[a[op_pos + 3]] = 0
+        end
+        op_pos += 4
+    elsif op == "08"
+        if first == second
+            a[a[op_pos + 3]] = 1
+        else
+            a[a[op_pos + 3]] = 0
+        end
+        op_pos += 4
     end
 end
